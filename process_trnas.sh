7e5fe101c538451fcb45c4b548e892270c7e666f
Samee Mushtak <mushtak.s@northeastern.edu>
2024-10-06 17:17:17 -0400

    Added MAFFT to process_trnas.sh

--- process_trnas.sh
+++ process_trnas.sh
@@ -29,9 +29,20 @@ while read label url; do
     rm $working_dir/$fasta_fname
     mv $working_dir/$fasta_fname-e $working_dir/$fasta_fname
 
-    # TODO: Automatically remove docker containers that are created by this command
-    container_name=$(date "+%Y%m%d_%H%M%S")-$label-clustalo
     container_dir=$PWD/$working_dir
-    docker run --platform linux/amd64 --name $container_name -v $container_dir:/data biocontainers/clustal-omega:v1.2.1_cv5 clustalo --infile=$fasta_fname --guidetree-out=$fasta_prefix-guidetree.dnd --outfmt=clustal --resno --wrap=150 --output-order=input-order --outfile=$fasta_prefix.clustal_num --seqtype=RNA
-    ete3 view -t $working_dir/$fasta_prefix-guidetree.dnd -i $working_dir/$fasta_prefix-guidetree.png
+    clustalo_container_name=$(date "+%Y%m%d_%H%M%S")-$label-clustalo
+    clustalo_dir=$working_dir/clustal-omega
+    mkdir -p $clustalo_dir
+    # TODO: Automatically remove docker containers that are created by this command
+    docker run --platform linux/amd64 --name $clustalo_container_name -v $container_dir:/data biocontainers/clustal-omega:v1.2.1_cv5 clustalo --infile=$fasta_fname --guidetree-out=clustal-omega/$fasta_prefix-guidetree.dnd --outfmt=clustal --resno --wrap=150 --output-order=input-order --outfile=clustal-omega/$fasta_prefix.clustal_num --seqtype=RNA
+    ete3 view -t $clustalo_dir/$fasta_prefix-guidetree.dnd -i $clustalo_dir/$fasta_prefix-guidetree.png
+
+    mafft_container_name=$(date "+%Y%m%d_%H%M%S")-$label-mafft
+    mafft_dir=$working_dir/mafft
+    mkdir -p $mafft_dir
+    docker run --platform linux/amd64 --name $mafft_container_name -v $container_dir:/data biocontainers/mafft:v7.407-2-deb_cv1 mafft --inputorder --anysymbol --kimura 1 --auto --treeout $fasta_fname > $mafft_dir/$fasta_prefix-aligned.fasta
+    # FastTree -nt -gtr -gamma $mafft_dir/$fasta_prefix-aligned.fasta > $mafft_dir/$fasta_prefix.tre
+    mv $working_dir/$fasta_fname.tree $mafft_dir/$fasta_fname.tree
+    ete3 view -t $mafft_dir/$fasta_fname.tree -i $mafft_dir/$fasta_prefix-tree.png
+
 done < $input_trnas
