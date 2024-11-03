98c6a42ed0a933f7dffa89faa60e65cd03ac0aca
Samee Mushtak <mushtak.s@northeastern.edu>
2024-11-03 18:13:51 -0500

    Improvements to clustal->json conversion

--- src/msa_latex/clustal_to_json.py
+++ src/msa_latex/clustal_to_json.py
@@ -1,20 +1,50 @@
+import json
 import argparse
 import msa_latex_utils as msalu
 
+def print_json_file(alignment_file):
+    alignment = msalu.read_alignment_file(alignment_file)
+
+    mapper = { 'A':0, 'T':1, 'U':1, 'C':2, 'G':3, 'B':4, 'N':4 }
+
+    data = []
+    for i in range(alignment.alignment_len):
+        base_num = 1
+        for j in range(alignment.alignment_width):
+            pt = {}
+            base = alignment.seqs[i].seq[j]
+            if base != '-':
+                pt['seq_idx'] = i
+                pt['seq_label'] = alignment.seqs[i].label
+                pt['base_idx'] = j+1
+                pt['base_label'] = base
+                pt['base_color'] = mapper[base]
+                pt['base_num'] = base_num
+                data.append(pt)
+                base_num += 1
+
+    for i in range(alignment.alignment_width):
+        consensus_base = alignment.seqs[0].seq[i]
+        consensus = True
+        for j in range(alignment.alignment_len):
+            pt = {}
+            if consensus and alignment.seqs[j].seq[i] != consensus_base:
+                consensus = False
+            if j == alignment.alignment_len - 1:
+                pt['seq_idx'] = alignment.alignment_len
+                pt['seq_label'] = 'Consensus'
+                pt['base_idx'] = i+1
+                pt['base_label'] = '⏺' if consensus else ' '
+                pt['base_color'] = -1
+                pt['base_num'] = i+1
+                data.append(pt)
+    print(json.dumps(data, ensure_ascii=False))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help='Path to input alignment file in clustal format')
-parser.add_argument('-s', '--species', help='Species of the tRNA in the alignemnt')
-parser.add_argument('-a', '--algorithm', help='Algorithm used to produce the alignment')
 args = parser.parse_args()
 
 if not args.file:
     raise RuntimeError("File name is required")
 
-# if not args.species:
-    # raise RuntimeError("Species name is required")
-
-# if not args.algorithm:
-    # raise RuntimeError("Algorithm name is required")
-
-config = msalu.JsonConfig(args)
-msalu.print_json_file(config)
+print_json_file(args.file)
