7a53380b34d44e0f81d7cd7f4341263209abc4f7
Samee Mushtak <mushtak.s@northeastern.edu>
2024-11-17 12:13:31 -0500

    Improvements to extract script. Using isotype specific model.

--- eda/t-loop-analysis/extract_type_ii.sh
+++ eda/t-loop-analysis/extract_type_ii.sh
@@ -2,21 +2,41 @@
 
 fasta_urls=fasta_urls.txt
 fastas_dir=source_fastas
+alns_dir=gtrnadb_alns
+cms_dir=cms
+
+if [[ $# -ne 1 ]]; then
+    echo "Exactly one arugment should be passed" >&2
+    exit 2
+fi
+
+if [[ $1 != "A" && $1 != "B" && $1 != "F" ]]; then
+    echo "Argument must be either 'A', 'B', or 'F'" >&2
+    exit 2
+fi
 
 # DOWNLOAD FASTAS
-# while read url; do
-    # Take all characters after the last forward slash to be the filename
-    # fasta_fname=${url##*/}
-    # curl --output $fastas_dir/$fasta_fname $url
+if [[ $1 == "A" || $1 == "F" ]]; then
+    while read url; do
+        # Take all characters after the last forward slash to be the filename
+        fasta_fname=${url##*/}
+        curl --output $fastas_dir/$fasta_fname $url
+    done < $fasta_urls
+fi
 
-# done < $fasta_urls
-
-for aa in Leu Ser; do
-    # EXTRACT BY ISOTYPE
-    awk "/tRNA-$aa/ {n=3} n-- > 0 {if (n==1) {printf} else {print \$1}}" $fastas_dir/*.fa > $aa.fa
-    # ALIGN
-    cmalign archaea.cm $aa.fa | awk "/^[A-Z]/ {print \$1; print \$2}; /^#=GR/ {print \$4} /^#=GC/ {print \$3}" > $aa-aligned.txt
-done
+if [[ $1 == "B" || $1 == "F" ]]; then
+    for aa in Leu Ser; do
+        # EXTRACT BY ISOTYPE
+        # Assuming each tRNA sequence takes up at most 2 rows or 2*60=120 base pairs
+        # This is not always the case, but these longer sequences should almost certainly be low quality
+        # and would be caught in manual review after the first pass
+        awk "/tRNA-$aa/ {n=3} n-- > 0 {if (n==1) {printf} else {print \$1}}" $fastas_dir/*.fa > $aa.fa
+        # BUILD COVARIANCE MODEL FROM STO ALIGNMENT FILE FROM GtRNAdb
+        cmbuild $cms_dir/arch-$aa.cm $alns_dir/arch-$aa.sto
+        # ALIGN
+        cmalign $cms_dir/arch-$aa.cm $aa.fa | awk "/^[A-Z]/ {print \$1; print \$2}; /^#=GR/ {print \$4} /^#=GC/ {print \$3}" > $aa-aligned.txt
+    done
+fi
 
 # Steps:
 # Download
